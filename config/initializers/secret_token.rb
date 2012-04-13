c77f7ae91f4d25ed7d2b5a26838bf1943cf5baf6
Norm Brogdon, Jr <norm@thebytestuff.com>
2012-04-13 17:54:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NormTest::Application.config.secret_token = '0f919527b6845157efd8441b120891dfea32ae8d17a6fe31c0e7ad1fd2fd3899672dea9fb575f0d54e8a996c6ec2e792fdb916bffb14ce0a60e7161e98786570'
