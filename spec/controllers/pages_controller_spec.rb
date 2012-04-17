90d160cc04798472c08b4e7b20d113da8f4f9b8c
Norm Brogdon, Jr <norm@thebytestuff.com>
2012-04-17 10:47:37 -0600

    added site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+      :content => "The Byte Stuff | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,12 +26,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+      :content => "The Byte Stuff | Contact")
+    end
   end
 
   describe "GET 'product'" do
     it "should be successful" do
       get 'product'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'product'
+      response.should have_selector("title",
+      :content => "The Byte Stuff | Product")
     end
   end
 
